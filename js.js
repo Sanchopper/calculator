7bb48b41a850a978f0627b4997997facffef2ad5
Sanchopper <sonjik001@gmail.com>
2025-04-24 19:29:38 +0500

    no need to press AC after every calculation

--- js.js
+++ js.js
@@ -2,15 +2,23 @@ let currentNumber = ''
 let firstNumber = ''
 let OP
 let monitor = document.querySelector('.monitor')
+let justCalculated = false
 
 
 const digits = document.querySelectorAll(".dgt")
 digits.forEach((button) => {
     button.addEventListener('click', ()=> {
+        if (justCalculated){
+            currentNumber = ''
+            monitor.textContent = ''
+            justCalculated = false
+        }
         currentNumber += button.id
         monitor.textContent = currentNumber
     })
 })
+
+
  const operator = document.querySelectorAll('.op')
  operator.forEach((button) => {
     button.addEventListener('click', ()=> {
@@ -21,12 +29,16 @@ digits.forEach((button) => {
     })
  })
 
+
  const calculate = document.querySelector('.calculate')
  calculate.addEventListener('click', ()=> {
     let firstNumberFloat = Number(firstNumber)
     let currentNumberFloat = Number(currentNumber)
-    monitor.textContent = calculation(firstNumberFloat, currentNumberFloat, OP)
-
+    let result = calculation(firstNumberFloat, currentNumberFloat, OP)
+    monitor.textContent = result
+    firstNumber = result.toString()
+    currentNumber = ''
+    justCalculated = true
  })
 
 const clear = document.querySelector('.clear')
@@ -35,21 +47,19 @@ clear.addEventListener('click', ()=> {
 })
 
  function calculation(a, b, op) {
-    if (op === '/') {
-        return a / b
-    } else if (op === '*'){
-        return a * b
-    } else if (op === '+'){
-        return a + b
-    } else {
-        return a - b
+    if (op === '/' && b === 0) {
+        return "error"
     }
-
+    if (op === '/') return a / b
+    if (op === '*') return a * b
+    if (op === '+') return a + b
+    return a - b
  }
 
  function clearCalculator(){
-    firstNumber = null;
+    firstNumber = '';
     currentNumber = '';
     OP = null;
     monitor.textContent = '0';
+    justCalculated = false
  }
