693bb4713decddec459224961cfbbe48d4c4ad5d
Sanchopper <sonjik001@gmail.com>
2025-04-24 16:38:00 +0500

    added functionality

--- js.js
+++ js.js
@@ -1,10 +1,41 @@
-const buttons = document.querySelectorAll("button")
-buttons.forEach((button) => {
+let currentNumber = ''
+let firstNumber
+let OP
+let monitor = document.querySelector('.monitor')
+
+
+const digits = document.querySelectorAll(".dgt")
+digits.forEach((button) => {
     button.addEventListener('click', ()=> {
-        
+        currentNumber += button.id
+        monitor.textContent = currentNumber
     })
 })
+ const operator = document.querySelectorAll('.op')
+ operator.forEach((button) => {
+    button.addEventListener('click', ()=> {
+        firstNumber = currentNumber
+        currentNumber = ''
+        OP = button.id
+    })
+ })
 
-let firstOP = 0
-let secondOP
-let expression
+ const calculate = document.querySelector('.=')
+ calculate.addEventListener(('click', ()=> {
+    let firstNumber = num(str)
+    let currentNumber = num(str)
+    monitor.textContent = calculation(firstNumber, currentNumber, OP)
+ }))
+
+ function calculation(a, b, op) {
+    if (op === '/') {
+        return a / b
+    } else if (op === '*'){
+        return a * b
+    } else if (op === '+'){
+        return a + b
+    } else {
+        return a - b
+    }
+
+ }
